6163d06916daadd05712f4d567685bb67797fe73
eegbroko <eegbroko@gmail.com>
2023-08-15 14:39:07 +0000

    size of function determination

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - printf to display sizes
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+	printf("The size of an char is: %lu. \n", (unsigned long)sizeof(a));
+	printf("The size of an int is: %lu. \n", (unsigned long)sizeof(b));
+	printf("The size of an long int is: %lu.\n", (unsigned long)sizeof(c));
+	printf("The size of an long long int is: %lu.\n", (unsigned long)sizeof(d));
+	printf("The size of an float is: %lu. \n", (unsigned long)sizeof(f));
+return (0);
+}
+
