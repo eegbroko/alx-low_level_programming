87839ab1b9fe2d8a6bd5a4b9cbae105d14c599d8
eegbroko <eegbroko@gmail.com>
2023-08-19 15:20:56 +0000

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point 2-print_alphabet.c
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 97;
+for (n = 97; n <= 122; n++)
+putchar(n);
+putchar('\n');
+return (0);
+}
