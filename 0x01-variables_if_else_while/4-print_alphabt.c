28d033dea09821b904feb02704e0dbd9fc4f4622
eegbroko <eegbroko@gmail.com>
2023-08-20 16:44:53 +0000

    4-print_alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print positive or negative
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 97;
+for (n = 97; n <= 122;)
+continue;
+
+for (; n == 101 || n == 113; n++)
+
+putchar('\n');
+return (0);
+}
