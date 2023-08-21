e6de1bede4b95f5be2bc20335fb2374d14de9f6a
eegbroko <eegbroko@gmail.com>
2023-08-21 18:25:17 +0000

    5-print_numbers

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,15 +9,13 @@
  */
 int main(void)
 {
-int i;
-
+int i = 97;
 for (i = 97; i <= 122; i++)
-
-	if (i == 101 || i == 113)
-	{
-		continue;
-	}
 putchar(i);
-
+{
+if (i == 101 || i == 113)
+continue;
+}
+putchar(i);
 return (0);
 }
