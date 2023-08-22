a4b7a612ed83b32c920536f456d770b8a5301f58
eegbroko <eegbroko@gmail.com>
2023-08-22 19:27:35 +0000

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - 6 print numberz
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
