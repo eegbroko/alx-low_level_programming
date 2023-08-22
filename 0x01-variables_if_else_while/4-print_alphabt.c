0bab593fa82830d55d3c9351f34378fe4eebd1a2
eegbroko <eegbroko@gmail.com>
2023-08-22 15:57:06 +0000

    4-print_alphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,13 +9,15 @@
  */
 int main(void)
 {
-int i = 97;
-for (i = 97; i <= 122; i++)
-putchar(i);
-{
-if (i == 101 || i == 113)
-continue;
-}
-putchar(i);
+	int i;
+	for (i = 97; i <= 122; i++)
+
+	{
+		if (i != 101 && i != 113)
+		{
+		putchar(i);
+		}
+	}
+	putchar('\n');
 return (0);
 }
