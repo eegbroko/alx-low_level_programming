e8e3596567ea6e56d75fe9dd11ce93b60e490c3c
eegbroko <eegbroko@gmail.com>
2023-08-22 22:04:42 +0000

    7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - print 7-print_tebahpla
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 122; i >= 97; i--)
+	{
+		if (i >= 97)
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
