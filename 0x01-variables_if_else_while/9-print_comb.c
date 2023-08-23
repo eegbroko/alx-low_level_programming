dff20f71bdffaf7c688e3e38fc3a50a3b24d5211
eegbroko <eegbroko@gmail.com>
2023-08-23 05:44:45 +0000

    9-print_comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - print 9-print_comb
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%d", i);
+	}
+		putchar('\n');
+		return (0);
+}
+
